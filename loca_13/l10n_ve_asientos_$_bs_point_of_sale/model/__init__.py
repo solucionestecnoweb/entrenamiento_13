40556a642e1a8c5aac7bbbe2f2d95e1eca8927fb
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-09-21 08:38:00 -0400

    Modulo localizacion contable

---
+++ loca_13/l10n_ve_asientos_$_bs_point_of_sale/model/__init__.py
@@ -0,0 +1,7 @@
+#from . import res_company_inherit
+#from . import account_move_inherit
+#from . import otros_modelos_inherit
+from . import pos_payment_inherit
+from . import pos_order_inherit
+from . import pos_session_inherit
+from . import pos_wizard_libro_ventas_inherit
